6bff38b8074061f46b3847c31f97d2099c23f8b9
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-25 14:56:52 +0530

    array using generics class

--- Array/Array/InputArray.cs
+++ Array/Array/InputArray.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Array
+namespace Day13_Array
 {
-    internal class InputArray
+    public class ArrayListUsingGenericClass<T>
     {
+        public T[] inputArray;
+        public ArrayListUsingGenericClass(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void ToPrint()
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
     }
 }
